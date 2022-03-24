7f45962d3fd8e4c97d460802564dd5f136f31972
Matej Seifert <seifert@softonics.cz>
2022-03-24 21:05:34 +0100

    recursiveLevel fixed bug

--- src/main/java/pi/calc/Application.java
+++ src/main/java/pi/calc/Application.java
@@ -15,17 +15,16 @@ public class Application {
 
     private static final BigDecimal _r = BigDecimal.ONE; // Static radius = 1 
     private static final Long _interval = 10L; //Set timeout between calculactions in ms
-    private static final MathContext _round = new MathContext(100000); //Number of decimals 
+    private static final MathContext _round = new MathContext(10000); //Number of decimals 
     private BigInteger recursiveLevel = BigInteger.ZERO; //BigInteger -> If someone wants to tryhard Pi calculations
     
     public Application() {
         BigDecimal initP = BigDecimal.valueOf(1.0).pow(2, this._round).add(BigDecimal.valueOf(1.0).pow(2, this._round)).sqrt(this._round); //sqrt(1^2+1^2)
-        System.err.println(initP);
-        CalcPI(initP, BigInteger.valueOf(4L));
+        CalcPI(initP, BigInteger.valueOf(4L)); //Initial values sqrt(1^2+1^2) and 4 lines (square)
     }
 
     public void CalcPI(BigDecimal P, BigInteger countRec) {
-        this.recursiveLevel.add(BigInteger.ONE); //We are running this function
+        this.recursiveLevel = this.recursiveLevel.add(BigInteger.ONE); //We are running this function
         
         BigDecimal Rx = this._r.pow(2, this._round)
                 .subtract(P.divide(BigDecimal.valueOf(2.0), this._round).pow(2, this._round), this._round)
@@ -37,7 +36,7 @@ public class Application {
 
         BigDecimal pi = b.multiply(new BigDecimal(countRec), this._round)
                 .divide(_r, this._round); //b * countRec / _r
-        System.err.println("¦ Recursive level count: " + recursiveLevel + " Lines: " + countRec + " Pi: " + pi.toString());
+        System.err.println("¦ Recursive level count: " + recursiveLevel.toString() + " Lines: " + countRec + " Pi: " + pi.toString());
 
         countRec = countRec.multiply(BigInteger.TWO);
         try {
