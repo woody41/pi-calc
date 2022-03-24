9e9ccf6ad9189c6d3ee69ebbf5ce7e8dbbc46af1
Matej Seifert <seifert@softonics.cz>
2022-03-24 20:56:21 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# pi-calc
-Automatic Pi calculator
+# Pi calculator
+
+This program automatically calculates Pi with user params
+
+## Installation
+
+Compile and run with maven
+
+## Usage
+
+Run program with maven. 
+Optionally change 
+
+```java
+private static final Long _interval //Sleep between calculations
+private static final MathContext _round //Number of decimals that you will work with
+```
+
+## Contributing
+Pull requests are welcome. Feel free to contribute any improvements!
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/main/java/pi/calc/Application.java
@@ -0,0 +1,51 @@
+package pi.calc;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.math.MathContext;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Application app = new Application();
+    }
+
+    private static final BigDecimal _r = BigDecimal.ONE; // Static radius = 1 
+    private static final Long _interval = 10L; //Set timeout between calculactions in ms
+    private static final MathContext _round = new MathContext(100000); //Number of decimals 
+    private BigInteger recursiveLevel = BigInteger.ZERO; //BigInteger -> If someone wants to tryhard Pi calculations
+    
+    public Application() {
+        BigDecimal initP = BigDecimal.valueOf(1.0).pow(2, this._round).add(BigDecimal.valueOf(1.0).pow(2, this._round)).sqrt(this._round); //sqrt(1^2+1^2)
+        System.err.println(initP);
+        CalcPI(initP, BigInteger.valueOf(4L));
+    }
+
+    public void CalcPI(BigDecimal P, BigInteger countRec) {
+        this.recursiveLevel.add(BigInteger.ONE); //We are running this function
+        
+        BigDecimal Rx = this._r.pow(2, this._round)
+                .subtract(P.divide(BigDecimal.valueOf(2.0), this._round).pow(2, this._round), this._round)
+                .sqrt(this._round);//sqrt(_r^2-(P/2)^2)
+        BigDecimal Ry = this._r.subtract(Rx, this._round);
+        BigDecimal b = Ry.pow(2, this._round)
+                .add(P.divide(BigDecimal.valueOf(2.0), this._round).pow(2, this._round), this._round)
+                .sqrt(this._round); //sqrt(Ry^2+(P/2)^2)
+
+        BigDecimal pi = b.multiply(new BigDecimal(countRec), this._round)
+                .divide(_r, this._round); //b * countRec / _r
+        System.err.println("¦ Recursive level count: " + recursiveLevel + " Lines: " + countRec + " Pi: " + pi.toString());
+
+        countRec = countRec.multiply(BigInteger.TWO);
+        try {
+            Thread.sleep(this._interval);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Application.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        CalcPI(b, countRec);
+    }
+
+}
